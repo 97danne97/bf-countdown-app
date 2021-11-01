b5379ff9d64a6f23d7dd2c644beffd2c9fa93926
Daniel Subasic <subasic97@gmail.com>
2021-11-01 17:44:55 +0100

    Requirements data is now located in a separate JSON file

--- src/views/Requirements.vue
+++ src/views/Requirements.vue
@@ -6,6 +6,7 @@
 
 <script>
 import RequirementsCard from '@/components/RequirementsCard.vue'
+import requirements from '@/json/requirements.json'
 
 export default {
   name: 'Requirements',
@@ -14,39 +15,7 @@ export default {
   },
   data () {
     return {
-      requirementTypes:
-      [
-        {
-          type: 'Minimum',
-          requirements: {
-            OS: '64-bit Windows 10',
-            'Processor (AMD)': 'AMD Ryzen 5 3600',
-            'Processor (Intel)': 'Core i5 6600K',
-            Memory: '8GB',
-            'Video Memory': '4GB',
-            'Graphics card (NVIDIA)': 'Nvidia GeForce GTX 1050 Ti',
-            'Graphics card (AMD)': 'AMD Radeon RX 560',
-            DirectX: '12',
-            'Online Connection Requirements': '512 KBPS or faster Internet connection',
-            'Hard-drive space': '100GB'
-          }
-        },
-        {
-          type: 'Recommended',
-          requirements: {
-            OS: '64-bit Windows 10',
-            'Processor (AMD)': 'AMD Ryzen 7 2700X',
-            'Processor (Intel)': 'Intel Core i7 4790',
-            Memory: '16GB',
-            'Video Memory': '8GB',
-            'Graphics card (NVIDIA)': 'Nvidia GeForce RTX 3060',
-            'Graphics card (AMD)': 'AMD Radeon RX 6600 XT',
-            DirectX: '12',
-            'Online Connection Requirements': '512 KBPS or faster Internet connection',
-            'Hard-drive space': '100GB SSD'
-          }
-        }
-      ]
+      requirementTypes: requirements
     }
   }
 }
